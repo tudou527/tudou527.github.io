62503dc144e68949686777415d01d1596ac70756
tudou527 <tudou527@gmail.com>
2015-05-29 18:08:54 +0800

    feat: add oneapi website

---
+++ source/config/config.ts
@@ -0,0 +1,14 @@
+export default {
+  npmClient: 'pnpm',
+  title: 'OneAPI - 一个 API 生产工具',
+  metas: [
+    {
+      name: 'keywords',
+      content: 'oneapi, swagger, api,'
+    },
+    {
+      name: 'description',
+      content: 'OneAPI 通过 AST 分析 Spring 项目源码（Spring Boot/MVC）生成前端消费所需的 API，提供的 CLI 工具也可以与 Git/SVN Hooks 结合帮助你实现 API 生产自动化。相比 Swagger，OneAPI 对后端零接入成本，不需要启动应用。'
+    },
+  ],
+};
